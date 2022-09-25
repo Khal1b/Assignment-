2565b88cf134db050cebe13904522e1d801ab92c
Khal1b <113544341+Khal1b@users.noreply.github.com>
2022-09-25 04:58:33 +0100

    Add files via upload

---
+++ Assignment1/src/model/Detail.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author User
+ */
+public class Detail {
+    private String fName;
+    private String lName;
+    private String dob;
+    private int Age;
+    private int tNumber;
+    private String eMail;
+    private String StreetL1;
+    private String StreetL2;
+    private String city;
+    private String country;
+    private String Affiliation;
+    private String major;
+    private String career;
+    private String degree;
+    private String degree1S;
+    private String degree1E;
+    private String degree2;
+    private String degree2s;
+    private String degree2e;
+    private String img;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public int gettNumber() {
+        return tNumber;
+    }
+
+    public void settNumber(int tNumber) {
+        this.tNumber = tNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getStreetL1() {
+        return StreetL1;
+    }
+
+    public void setStreetL1(String StreetL1) {
+        this.StreetL1 = StreetL1;
+    }
+
+    public String getStreetL2() {
+        return StreetL2;
+    }
+
+    public void setStreetL2(String StreetL2) {
+        this.StreetL2 = StreetL2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getDegree1S() {
+        return degree1S;
+    }
+
+    public void setDegree1S(String degree1S) {
+        this.degree1S = degree1S;
+    }
+
+    public String getDegree1E() {
+        return degree1E;
+    }
+
+    public void setDegree1E(String degree1E) {
+        this.degree1E = degree1E;
+    }
+
+    public String getDegree2() {
+        return degree2;
+    }
+
+    public void setDegree2(String degree2) {
+        this.degree2 = degree2;
+    }
+
+    public String getDegree2s() {
+        return degree2s;
+    }
+
+    public void setDegree2s(String degree2s) {
+        this.degree2s = degree2s;
+    }
+
+    public String getDegree2e() {
+        return degree2e;
+    }
+
+    public void setDegree2e(String degree2e) {
+        this.degree2e = degree2e;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
+    
+    
+
